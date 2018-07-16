96d2549a2dcf60b7bbeba7b094b09121971f8473
purvishoza <prvshoza@gmail.com>
2018-07-16 15:50:54 -0700

    firstcommit

---
+++ src/app/user.service.ts
@@ -0,0 +1,12 @@
+/*import {Injectable} from '@angular/core';
+
+@Injectable()
+export class UserService{
+
+  private _url: string = "/assests/data/group.json";
+
+  getUsers(){
+    return this.http.get(this._url);
+  }
+}
+*/

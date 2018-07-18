550f22fbb8f3af003dd4d999461f8d734b6c1b5c
Janak Soni <18238847+jsoni2@users.noreply.github.com>
2018-07-18 16:17:03 -0700

    Home Page Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RaciComponent } from './raci/raci.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    RaciComponent
 
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    
 
   ],
   providers: [],//UserService],

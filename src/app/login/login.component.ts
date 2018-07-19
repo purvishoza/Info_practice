1cee664667a911f1b52ccb7c3ad464fcd232bc27
purvishoza <prvshoza@gmail.com>
2018-07-19 14:33:04 -0700

    raci and login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AppModule } from '../app.module';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+    providers: [UserService]
 })
 export class LoginComponent implements OnInit {
+  logon;
 
-  constructor() { }
+  constructor(private userService:UserService, private route: Router) { }
 
   ngOnInit() {
+    this.logon = {
+        email:'',
+        password:''
+    };
+  }
+
+  login(){
+    this.userService.login(this.logon).subscribe(
+      response => {
+        this.route.navigate(['/raci']);
+      },
+      error => {
+        console.log('error', error);
+      }
+    );
+    this.logon.password = '';
+    this.logon.email = '';
   }
 
 }

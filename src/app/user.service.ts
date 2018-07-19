185ffbce182a7522103e6251b7b36e4a1636cbfa
purvishoza <prvshoza@gmail.com>
2018-07-19 15:32:54 -0700

    commented module

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { Response } from "@angular/http";
+//import { Response } from "@angular/http";
 
 @Injectable({
   providedIn: 'root'

6e07852447c576f74f4246c1fcd4f456ca964c59
purvishoza <prvshoza@gmail.com>
2018-07-19 11:22:14 -0700

    delete unneccessary things

--- src/app/user/signin/signin.component.ts
+++ src/app/user/signin/signin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../shared/user.service';
+// import { UserService } from '../../shared/user.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 

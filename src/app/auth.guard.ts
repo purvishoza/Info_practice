cbd3eda1a87dde20aa1adec2de18c9d1f6827702
Purvish Oza <32309037+purvishoza@users.noreply.github.com>
2018-07-24 23:13:57 -0700

    needs final touch

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -11,7 +11,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-      //if (localStorage.getItem('userToken') != null)
+      if (localStorage.getItem('token') != null)
     //return true;
     this.router.navigate(['/login']);
     return false;
